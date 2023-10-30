24af56bbc5ea0412d35ccf818d345d862e80fed3
areeb <areeb@gmail.com>
2023-10-30 22:47:17 +0600

    Moved images to Public

--- src/BlenderStillImages.jsx
+++ src/BlenderStillImages.jsx
@@ -1,48 +1,25 @@
 import './BlenderImages.css';
+import IMAGES from './assets/folder_data.json'
 import { useState, useEffect } from 'react';
 
 export default function StillImages() {
     const [images, setImages] = useState([]);
-    const getData = () => {
-        fetch('../src/assets/BlenderArts/folder_data.json', {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-            .then(function (response) {
-                console.log(response)
-                return response.json();
-            })
-            .then(function (myJson) {
-                console.log(myJson);
-                myJson = myJson.filter((img) => img.type==".jpg" );
-                for(let img of myJson){
-                    img.className = "image";
-                    img.path = `../src/assets/BlenderArts/${encodeURI(img.name)}${img.type}`;
-                    //console.log(img.path)
-                }
-                //myJson = myJson.map((img) => img.name = encodeURI(img.name))
-                setImages(myJson);
-            });
-    }
     useEffect(() => {
-        try {
-            getData();
+        let imgs = [...IMAGES];
+        for (let img of imgs) {
+            img.className = "image";
         }
-        catch (err) {
-            console.error("Could not retrieve images");
-        }
-    }, [])
-
-    function setClasses(e, key){
+        //console.log(imgs);
+        setImages(imgs);
+    }, []);
+    function setClasses(e, key) {
         e.preventDefault();
         //console.log(key);
         let imgs = [...images];
-        for(let img of imgs){
+        for (let img of imgs) {
             console.log(img);
-            if(img.key == key){
-                img.className = (img.className=="image")?"imageZoom":"image";
+            if (img.key == key) {
+                img.className = (img.className == "image") ? "imageZoom" : "image";
                 console.log(img.className);
                 break;
             }
@@ -55,7 +32,7 @@ export default function StillImages() {
         <h1>My Artworks</h1>
         {(!images.length) && <h3>Loading...</h3>}
         {(images) && (<section className="imagesContainer">
-            {images.map((img) => <div key={img.key} className={img.className} style={{ backgroundImage: `url(${require(img.path)})`}} data-time={img.creation_time} data-name={img.name} onClick={ (e)=>{setClasses(e, img.key)} }></div>)}
+            {images.map((img) => <div key={img.key} className={img.className} style={{ backgroundImage: `url(./BlenderArts/${encodeURI(img.name)}.jpg)` }} data-time={img.creation_time} data-name={img.name} onClick={(e) => { setClasses(e, img.key) }}></div>)}
         </section>)}
     </>);
 }
