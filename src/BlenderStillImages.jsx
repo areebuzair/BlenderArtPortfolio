0468abe811b88a29f2652b323ca137469d4af51a
areeb <areeb@gmail.com>
2023-10-31 09:30:09 +0600

    Minor fixes

--- src/BlenderStillImages.jsx
+++ src/BlenderStillImages.jsx
@@ -30,7 +30,7 @@ export default function StillImages() {
 
     return (<>
         <h1>My Artworks</h1>
-        <p>I am <b>J. M Areeb Uzair</b>. I use Blender as a hobby, and also to create assets for various projects of mine. I have been using this software since 2022. Here are some of my still render (video section is under construction).</p>
+        <p>I am <b>J. M Areeb Uzair</b>. I use Blender as a hobby, and also to create assets for various projects of mine. I have been using this software since 2022. Here are some of my still renders (video section is under construction).</p>
         {(!images.length) && <h3>Loading...</h3>}
         {(images) && (<section className="imagesContainer">
             {images.map((img) => <div key={img.key} className={img.className} style={{ backgroundImage: `url(./BlenderArts/${encodeURI(img.name)}.jpg)` }} data-time={img.creation_time} data-name={img.name} onClick={(e) => { setClasses(e, img.key) }}></div>)}
