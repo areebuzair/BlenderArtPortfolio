376e101263fa2ebae72fe717eeaa675f904540ea
areeb <areeb@gmail.com>
2023-11-02 22:39:50 +0600

    Added a href

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,7 @@
 export default function HomePage(){
-    return <h1>Home Page</h1>
+    return <>
+        <h1>Home Page</h1>
+        <a href="./Images">See Images</a><br/>
+        <a href="#">See Animations</a>
+    </>
 }
