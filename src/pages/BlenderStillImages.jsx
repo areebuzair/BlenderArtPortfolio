b44c97ed352af8db9bf5e4fa6b6982be005dcd97
areeb <areeb@gmail.com>
2023-11-15 20:37:30 +0600

    Lazy Load images

--- src/pages/BlenderStillImages.jsx
+++ src/pages/BlenderStillImages.jsx
@@ -9,10 +9,10 @@ function disableScroll() {
     let TopScroll = window.pageYOffset || document.documentElement.scrollTop;
     let LeftScroll = window.pageXOffset || document.documentElement.scrollLeft;
 
-        // if scroll happens, set it to the previous value
-        window.onscroll = function () {
-            window.scrollTo(LeftScroll, TopScroll);
-        };
+    // if scroll happens, set it to the previous value
+    window.onscroll = function () {
+        window.scrollTo(LeftScroll, TopScroll);
+    };
 }
 
 function enableScroll() {
@@ -29,19 +29,49 @@ export default function StillImages() {
         //console.log(imgs);
         setImages(imgs);
     }, []);
+
+    const loadImages = (image) => {
+        image.style.backgroundImage = image.dataset.src;
+    };
+
+    useEffect(() => {
+        let observer = new window.IntersectionObserver(function (entries, self) {
+            //console.log("entries", entries);
+            //console.log("self", self);
+            // iterate over each entry
+            entries.forEach((entry) => {
+                // process just the images that are intersecting.
+                // isIntersecting is a property exposed by the interface
+                if (entry.isIntersecting) {
+                    // custom function that copies the path to the img
+                    // from data-src to src
+                    loadImages(entry.target);
+                    // the image is now in place, stop watching
+                    self.unobserve(entry.target);
+                }
+            });
+        }, {
+            rootMargin: "400px",
+        });
+        let imgElements = document.querySelectorAll('.image');
+        for (let elem of imgElements) {
+            observer.observe(elem);
+        }
+    }, [images]);
+
     function setClasses(e, key) {
         e.preventDefault();
         //console.log(key);
         let imgs = [...images];
         for (let img of imgs) {
-            console.log(img);
+            //console.log(img);
             if (img.key == key) {
                 img.className = (img.className == "image") ? "imageZoom" : "image";
-                if(img.className == "imageZoom"){
+                if (img.className == "imageZoom") {
                     document.querySelector(':root').style.setProperty('--scrollBehavior', 'auto');
                     disableScroll();
                 }
-                else{
+                else {
                     enableScroll();
                     document.querySelector(':root').style.setProperty('--scrollBehavior', 'smooth');
                 }
@@ -54,11 +84,11 @@ export default function StillImages() {
     }
 
     return (<section>
-        <Navlinks/>
+        <Navlinks />
         <h1 id="SectionTitle">Images</h1>
         {(!images.length) && <h3>Loading...</h3>}
         {(images) && (<div className="imagesContainer">
-            {images.map((img) => <div key={img.key} className={img.className} style={{ backgroundImage: `url(./BlenderArts/${encodeURI(img.name)}.jpg)` }} data-time={img.upload_time} data-name={img.name} onClick={(e) => { setClasses(e, img.key) }}></div>)}
+            {images.map((img) => <div key={img.key} className={img.className} /*style={{ backgroundImage:  }}*/ data-time={img.upload_time} data-name={img.name} data-src={`url(./BlenderArts/${encodeURI(img.name)}.jpg)`} onClick={(e) => { setClasses(e, img.key) }}></div>)}
         </div>)}
     </section>);
 }
