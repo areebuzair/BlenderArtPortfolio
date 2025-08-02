726c5e2f3a8ad6b6f06b415a21c2bb976a1a3184
areeb <areeb@gmail.com>
2025-08-02 18:09:40 +0600

    Adds reduced motion preference settings

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,17 @@ import Navlinks from './components/Navigator'
 import './Blender.css'
 
 export default function App() {
+  const isReduced = window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
   const handleMouseMove = (e) => {
-    document.querySelector(":root").style.setProperty("--mouse-x", `${e.clientX}px`)
-    document.querySelector(":root").style.setProperty("--mouse-y", `${e.clientY}px`)
+    if (!isReduced) {
+      document.querySelector(":root").style.setProperty("--mouse-x", `${e.clientX}px`)
+      document.querySelector(":root").style.setProperty("--mouse-y", `${e.clientY}px`)
+    }
     // console.log(e.clientX)
   }
   return <>
     <BrowserRouter>
-      <main onMouseMove={(e)=>{handleMouseMove(e)}} className={('onmousemove' in document) && 'MouseAvailable'}>
+      <main onMouseMove={(e) => { handleMouseMove(e) }} className={('onmousemove' in document) && 'MouseAvailable'}>
         <Navlinks />
         <Routes>
           <Route path="/BlenderArtPortfolio/" element={<Home />} />
