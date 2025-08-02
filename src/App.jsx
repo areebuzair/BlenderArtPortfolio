11a3f227fdec9652be9dab0d8cd6148251518a2a
areeb <areeb@gmail.com>
2025-08-02 22:10:47 +0600

    Fixes bottom glow issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import Animations from './pages/BlenderAnimations'
 import Home from './pages/Home'
 import Navlinks from './components/Navigator'
 import './Blender.css'
+import { useEffect, useState } from 'react'
 
 export default function App() {
-  const isReduced = window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+  const [isReduced, setIsReduced] = useState(true);
+  useEffect(() => {
+    setIsReduced(window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true);
+  })
   const handleMouseMove = (e) => {
     if (!isReduced) {
       document.querySelector(":root").style.setProperty("--mouse-x", `${e.clientX}px`)
@@ -16,10 +20,10 @@ export default function App() {
   }
   return <>
     <BrowserRouter>
-      <main onMouseMove={(e) => { handleMouseMove(e) }} className={('onmousemove' in document) && 'MouseAvailable'}>
+      <main onMouseMove={(e) => { handleMouseMove(e) }} >
         <Navlinks />
         <Routes>
-          <Route path="/BlenderArtPortfolio/" element={<Home isReduced={isReduced}/>} />
+          <Route path="/BlenderArtPortfolio/" element={<Home isReduced={isReduced} />} />
           <Route path="/BlenderArtPortfolio/Images" element={<StillImages />} />
           <Route path="/BlenderArtPortfolio/Videos" element={<Animations />} />
         </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Products from '../components/Products'
 import GALLERY from '../assets/Gallery.webp'
 import '../Home.css'
 
-export default function Home({isReduced}) {
+export default function Home({ isReduced }) {
 
   const vidControl = (e) => {
     const video = e.target;
@@ -21,7 +21,7 @@ export default function Home({isReduced}) {
 
   return (
     <section className='home'>
-      <Introduction isReduced={isReduced}/>
+      <Introduction isReduced={isReduced} />
       {/* <Products /> */}
       <div className='home-articles-container'>
         <article>
@@ -31,13 +31,13 @@ export default function Home({isReduced}) {
         </article>
         <article>
           <h2>Animations</h2>
-          <div className='home-articles-media-container'><video src="https://dl.dropbox.com/scl/fi/cfon2k6hvu6gp4jmqm07h/Mobius-Strip.mp4?rlkey=6a8b32o55kedarjwj2o5d7g7l&dl=0" onClick={vidControl} loop={true} controlsList="nodownload" onContextMenu={(e) => { e.preventDefault(); }} /></div>
+          <div className='home-articles-media-container'><video src="https://dl.dropbox.com/scl/fi/cfon2k6hvu6gp4jmqm07h/Mobius-Strip.mp4?rlkey=6a8b32o55kedarjwj2o5d7g7l&dl=0" onClick={vidControl}  controlsList="nodownload" onContextMenu={(e) => { e.preventDefault(); }} /></div>
           <p>Check out some of my <a href="./Videos">animations</a>.</p>
         </article>
         <article className='two-column'>
           <h2>My Addons</h2>
-          <div className='home-articles-media-container'><video src="./Assets/MPB_Demo.mkv" onClick={vidControl} loop={true} controlsList="nodownload" onContextMenu={(e) => { e.preventDefault(); }} /></div>
-          <p>Check out my Addons on GitHub! Compatible with Blender 4.1 and above (<i>tested upto 4.4</i>)
+          <div className='home-articles-media-container'><video src="./Assets/MPB_Demo.mp4" onClick={vidControl} loop={true} controlsList="nodownload" onContextMenu={(e) => { e.preventDefault(); }} /></div>
+          <p>Check out my Addons on GitHub! Compatible with Blender 4.1 and above (<i>tested upto 4.4</i>).
             <ul style={{ paddingLeft: "2vmin", listStyleType: "none" }}>
               <li><a href="https://github.com/areebuzair/mesh-particle-blend" target='_blank'>Mesh Particle Blend:</a> A Blender addon that creates a point cloud that blends between two meshes using Geometry Nodes.</li>
               <li><a href="https://github.com/areebuzair/blender-plugin" target='_blank'>Sketchfab Plugin:</a> A fork of the official Sketchfab addon which automatically generates an attribution file for imported models.</li>
@@ -48,7 +48,7 @@ export default function Home({isReduced}) {
       <div className='mainVideo'>
         <h2>First Flight</h2>
         <h5>First Short Film</h5>
-        <video controls controlslist="nodownload" src="https://dl.dropbox.com/scl/fi/xu43vxw3n7nwp1poz11pw/First-Flight.mp4?rlkey=zi7z1bzeyvye2anewaxryy42a&amp;dl=0" onContextMenu={(e)=>e.preventDefault()}></video>
+        <video controls controlsList="nodownload" src="https://dl.dropbox.com/scl/fi/xu43vxw3n7nwp1poz11pw/First-Flight.mp4?rlkey=zi7z1bzeyvye2anewaxryy42a&amp;dl=0" onContextMenu={(e) => e.preventDefault()}></video>
       </div>
     </section>
   )
