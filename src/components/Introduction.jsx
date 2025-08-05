af25e7096ce5ade65202e950e067c69eb20e8a9e
areeb <areeb@gmail.com>
2025-08-05 23:34:19 +0600

    Added alt text

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,4 +1,4 @@
-export default function Introduction({isReduced}) {
+export default function Introduction({ isReduced }) {
     return (<header className="Introduction">
         <div className="logoImage">
             {!isReduced && <iframe
@@ -6,9 +6,10 @@ export default function Introduction({isReduced}) {
             ></iframe>}
             {isReduced && <img
                 src="/BlenderArtPortfolio/SVGs/Double A Logo SVG.svg"
+                alt="DoubleA Logo"
             />}
         </div>
         <p>I am <b>J. M Areeb Uzair</b>. I use Blender as a hobby, and also to create assets for various projects of mine. I have been using this software since 2022. You can see some of my finished projects here.</p>
-        <span>Catch my other projects here: <a href="../" id="HUBlink" style={{color: "#00EEFF"}}>Project HUB{'\u2197'}</a></span>
+        <span>Catch my other projects here: <a href="../" id="HUBlink" style={{ color: "#00EEFF" }}>Project HUB{'\u2197'}</a></span>
     </header>)
 }
